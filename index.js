554fb5b04d1f1f8357d10b14e154b49a769764e1
Arnold <maniraguhaarnold738@gmail.com>
2024-05-09 02:32:30 +1200

    commited

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('hello word');
+// Varriable declaration and initialization 
+let Myname = 'Az';
+// Display the value stored in variable
+console.log(Myname);
+// Constant value
+const pi=3.14;
+console.log(pi);
+let = {firstname:eric
+
+}
+
+
+
---
+++ myjavascript.js
@@ -0,0 +1,12 @@
+function openwindow(){
+    var myName = document.getElementById("username")
+    var myPassword = document.getElementById("password")
+    if(myName.value=== "admin"&& myPassword.value==="1234")
+    {
+        alert("welcome");
+    }
+    else
+    {
+        alert("sorry");
+    }
+} 
